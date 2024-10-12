69a8c70619d2432218b5d97bbc4d271a5fefd7cb
Marek Zelinka <mzelinka17@gmail.com>
2024-10-12 18:16:52 +0200

    add index page

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
-import { route, type RouteConfig } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
 export const routes: RouteConfig = [
+  index("./routes/home.tsx"),
   route("contacts/:contactId", "./routes/contact.tsx"),
   route("contacts/:contactId/edit", "./routes/edit.tsx"),
   route("contacts/:contactId/destroy", "./routes/destroy.tsx"),
