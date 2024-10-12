aa6a2987d82a1d197ad51bc7dac6abfc6785a729
Marek Zelinka <mzelinka17@gmail.com>
2024-10-12 18:18:18 +0200

    fix types

--- app/routes/destroy.tsx
+++ app/routes/destroy.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "react-router";
 import { deleteContact } from "~/data";
-import type * as Route from "./+types.contact";
+import type * as Route from "./+types.destroy";
 
 export async function action({ params }: Route.ActionArgs) {
   await deleteContact(params.contactId);
