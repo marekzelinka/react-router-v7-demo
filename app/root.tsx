129cc8142d05398816b0003eb1af6ac09e645665
Marek Zelinka <mzelinka17@gmail.com>
2024-10-12 16:41:11 +0200

    setup for tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import type { LinksFunction } from "react-router";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-
+import { Form, Links, Meta, Scripts, ScrollRestoration } from "react-router";
 import "./app.css";
 
 export const links: LinksFunction = () => [
@@ -35,5 +34,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <div id="sidebar">
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">{/* <Outlet /> */}</div>
+    </>
+  );
 }
